9827192ab24441407ac885d3e7514c4b7b4d7757
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-29 16:55:15 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,35 @@
 class Owner
-  # code goes here
 
- end
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+attr_reader :species
+attr_accessor :name, :pets
+
+  def buy_fish(name)
+    @pets[:fishes] << name
+  end
+  
+  def buy_cat(name)
+    @pets[:cats] << name
+  end
+  
+  def buy_dog(name)
+    @pets[:dogs] << name
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+  
+end
+
+owner = Owner.new("human")
+puts owner.say_species
+# puts owner.question
