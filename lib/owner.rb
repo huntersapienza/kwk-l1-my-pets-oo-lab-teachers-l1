d8426196b78433693693e2791220165a15c703ad
Hunter Sapienza <sapienzahunter@gmail.com>
2018-07-29 16:54:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -21,7 +21,7 @@ attr_accessor :name, :pets
   end
 
   def list_pets
-    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length}, cat(s)."
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
 
   def say_species
